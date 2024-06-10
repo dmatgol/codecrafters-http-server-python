6d1c5710351ab244fb5455e0ccb464ee47992fbd
Diogo Matias <diogo.pomatias@gmail.com>
2024-06-10 22:37:39 +0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@
 import socket
 
 
+
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -24,12 +27,26 @@ def parse_request(request_data: bytes):
     decoded_response = request_data.decode('utf-8')
     request_line = decoded_response.split('\r\n')[0]
     method, path, http_version = request_line.split()
+    print(path == "/")
     if path == "/":
         response = b"HTTP/1.1 200 OK\r\n\r\n"
+    elif "echo" in path:
+        response = generate_response(path, http_version)
     else:
         response = b"HTTP/1.1 404 Not Found\r\n\r\n"
     return response
 
+
+def generate_response(path: str, http_version):
+    status_code = "200 OK\r\n"
+    content_type = "Content-Type: text/plain\r\n"
+    content = path.split('/')[-1]
+    content_len = f'Content-Length: {len(content)}\r\n\r\n'
+    response = f"{http_version} {status_code}{content_type}{content_len}{content}"
+    print(response)
+    response = response.encode('utf-8')
+    return response
+                
 
 
 
